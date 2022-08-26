1efc92f2e9aa5e21f1c5d9aff7bd57751847084e
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-08-26 08:26:16 +0100

    add mysql connect

--- server/run.py
+++ server/run.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL, MySQLdb
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return app.send_static_file('index.html')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'nztunnellers_v2'
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-if __name__ == '__main__': 
-    app.run()
+mysql = MySQL(app)
+
+
+@app.route("/")
+def index():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT id, surname, forename, serial FROM company_roll")
+    rv = cur.fetchall()
+    tunnellers = []
+    content = {}
+    for result in rv:
+        content = {'id': result['id'], 'forename': result['forename'],
+                   'surname': result['surname'], 'serial': result['serial']}
+        tunnellers.append(content)
+        content = {}
+    return jsonify(tunnellers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
