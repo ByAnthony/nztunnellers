fbd7ca5241cadb40f20ef443540df8ebc9cd905c
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-08-26 08:48:14 +0100

    mysql connect locally

--- server/run.py
+++ server/run.py
@@ -3,10 +3,11 @@ from flask_mysqldb import MySQL, MySQLdb
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'nztunnellers_v2'
+app.config["MYSQL_HOST"] = "127.0.0.1"
+app.config["MYSQL_PORT"] = 8889
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "nztunnellers_v2"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
