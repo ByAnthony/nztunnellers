098620b3a5f77878eba94f26c4cee4808bddf4d6
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-03 16:59:51 +0100

    add embarkation_unit and training fields

--- server/mapper/date_mapper.py
+++ server/mapper/date_mapper.py
@@ -1,6 +1,13 @@
+import time
+
+
 def map_birth(year, country):
     return {'year': year, 'country': country}
 
 
 def format_year(date):
     return date.strftime('%Y')
+
+
+def format_date(date):
+    return date.strftime('%Y-%m-%d')
--- server/mapper/tunneller_mapper.py
+++ server/mapper/tunneller_mapper.py
@@ -1,6 +1,6 @@
 import datetime
 from mapper.parent_mapper import map_parent
-from mapper.date_mapper import map_birth, format_year
+from mapper.date_mapper import map_birth, format_year, format_date
 from mapper.nz_archives_mapper import map_nz_archives
 
 
@@ -25,7 +25,15 @@ def map_tunneller(response):
             },
             'military_life': {
                 'serial': data['serial'],
-                'rank': data['rank_en']
+                'rank': data['rank_en'],
+                'embarkation_unit': {
+                    'embarkation_unit': data['embarkation_unit_en'],
+                    'training': {
+                        'training_start': format_date(data['training_start']),
+                        'training_location': data['training_location'],
+                        'training_location_type': data['training_location_type_en']
+                    },
+                },
             },
             'sources': {
                 'nz_archives': map_nz_archives(data['nz_archives_ref_1'], data['nz_archives_url_1'], data['nz_archives_ref_2'], data['nz_archives_url_2']),
