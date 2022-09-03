d5b31c82f28595c595f0f7850a6a70fc08455a6b
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-03 15:53:35 +0100

    added new fields to tunneller mapper

--- server/mapper/tunneller_mapper.py
+++ server/mapper/tunneller_mapper.py
@@ -1,21 +1,60 @@
+import datetime
+
+
 def map_tunneller(response):
     tunneller = None
     for data in response:
         tunneller = {
             'id': data['id'],
-            'forename': data['forename'],
-            'surname': data['surname'],
-            'serial': data['serial'],
-            'birth_country': data['birth_country_en'],
-            'parents': {
-                'mother_name': data['mother_name'],
-                'mother_origin': data['mother_origin_en'],
-                'father_name': data['father_name'],
-                'father_origin': data['father_origin_en']
+            'name': {
+                'forename': data['forename'],
+                'surname': data['surname'],
+                'aka': data['aka']
             },
-            'pre-war': {
+            'parents': {
+                'mother': map_parent(data['mother_name'], data['mother_origin_en']),
+                'father': map_parent(data['father_name'], data['father_origin_en'])
+            },
+            'pre_war': {
+                'birth': map_birth(format_year(data['birth_date']), data['birth_country_en']),
                 'religion': data['religion_en'],
                 'marital_status': data['marital_status_en']
+            },
+            'military_life': {
+                'serial': data['serial'],
+                'rank': data['rank_en']
+            },
+            'sources': {
+                'nz_archives': map_nz_archives(data['nz_archives_ref_1'], data['nz_archives_url_1'], data['nz_archives_ref_2'], data['nz_archives_url_2']),
+                'awmm_cenotaph': data['awmm_cenotaph'],
+                'nominal_roll': {
+                    'volume': data['nominal_roll_volume'],
+                    'roll': data['nominal_roll_number'],
+                    'page': data['nominal_roll_page']
+                },
             }
         }
     return tunneller
+
+
+def map_parent(name, origin):
+    return {'name': name, 'origin': origin}
+
+
+def map_birth(year, country):
+    return {'year': year, 'country': country}
+
+
+def map_nz_archives(ref_1, url_1, ref_2, url_2):
+    if (ref_2 is not None and url_2 is not None):
+        return [
+            {'ref': ref_1, 'url': url_1},
+            {'ref': ref_2, 'url': url_2}
+        ]
+    return [
+        {'ref': ref_1, 'url': url_1}
+    ]
+
+
+def format_year(date):
+    return date.strftime('%Y')
--- server/repositories/tunneller_repository.py
+++ server/repositories/tunneller_repository.py
@@ -3,15 +3,17 @@ from db.run_sql import run_sql
 
 def show(id, mysql):
     sql = f'''
-        SELECT id, surname, forename, serial, birth_country_fk, birth_country.country_en AS birth_country_en, mother_name, mother_origin_fk, mother_origin.country_en AS mother_origin_en, father_name, father_origin_fk, father_origin.country_en AS father_origin_en, marital_status.marital_status_en, religion.religion_en
+        SELECT id, surname, forename, aka, serial, rank_fk, rank_en, birth_date, birth_country_fk, birth_country.country_en AS birth_country_en, mother_name, mother_origin_fk, mother_origin.country_en AS mother_origin_en, father_name, father_origin_fk, father_origin.country_en AS father_origin_en, marital_status.marital_status_en, religion.religion_en, nz_archives_ref_1, nz_archives_url_1, nz_archives_ref_2, nz_archives_url_2, awmm_cenotaph, nominal_roll_fk, nominal_roll.nominal_roll_volume, nominal_roll.nominal_roll_number, nominal_roll.nominal_roll_page
 
         FROM tunneller t
 
+        LEFT JOIN rank ON t.rank_fk=rank.rank_id
         LEFT JOIN country birth_country ON t.birth_country_fk=birth_country.country_id
         LEFT JOIN country mother_origin ON t.mother_origin_fk=mother_origin.country_id
         LEFT JOIN country father_origin ON t.father_origin_fk=father_origin.country_id
         LEFT JOIN marital_status ON t.marital_status_fk=marital_status.marital_status_id
         LEFT JOIN religion ON t.religion_fk=religion.religion_id
+        LEFT JOIN nominal_roll ON t.nominal_roll_fk=nominal_roll.nominal_roll_id
 
         WHERE id=%s
 
