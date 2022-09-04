73043e7112a7ea34b47c367eb6d213c1440afaeb
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-04 08:32:44 +0100

    refactor roll sql query

--- server/repositories/roll_repository.py
+++ server/repositories/roll_repository.py
@@ -2,9 +2,10 @@ from db.run_sql import run_sql
 
 
 def select_all(mysql):
-    sql = f'''SELECT id, surname, forename, serial
-            FROM tunneller t
-            ORDER BY surname, forename
-        '''
+    sql = f'''
+        SELECT id, surname, forename, serial
+        FROM tunneller t
+        ORDER BY surname, forename
+    '''
     results = run_sql(sql, mysql)
     return results
