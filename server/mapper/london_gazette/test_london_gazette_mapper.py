959adbc8c009ffbe2c51c0f0f60159d902a6fd64
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-13 20:20:35 +0100

    add test for london_gazette mapper

---
+++ server/mapper/london_gazette/test_london_gazette_mapper.py
@@ -0,0 +1,23 @@
+from datetime import date
+from . import london_gazette_mapper
+
+
+london_gazette_list = ({'london_gazette_date': date(1917, 12, 28), 'london_gazette_page': '13575'}, {
+    'london_gazette_date': date(1918, 1, 1), 'london_gazette_page': '29'})
+
+
+def test_if_london_gazette_is_not_none_then_returns_map_london_gazette():
+    assert london_gazette_mapper.map_london_gazette(london_gazette_list) == [
+        {
+            "date": "1917-12-28",
+            "page": "13575"
+        },
+        {
+            "date": "1918-01-01",
+            "page": "29"
+        }
+    ]
+
+
+def test_if_london_gazette_is_none_then_returns_empty_list():
+    assert london_gazette_mapper.map_london_gazette(()) == []
