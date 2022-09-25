420ce694ea6ccb362bcda26420302f5961c626d3
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-25 21:02:59 +0100

    remove bracket

--- server/mapper/month_year/month_year_mapper.py
+++ server/mapper/month_year/month_year_mapper.py
@@ -5,7 +5,7 @@ year_col = {'en': 'years', 'fr': 'ans'}
 def month_year_mapper(month, lang):
     if month is not None:
         if int(month) < int(24):
-            return '{} {}}'.format(month, month_col[lang])
+            return '{} {}'.format(month, month_col[lang])
         if int(month) >= int(24):
             result = int(month) // int(12)
             return '{} {}'.format(result, year_col[lang])
