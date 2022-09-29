8f7ef9df112a91c9b7a2e2398341b4635e49e3f4
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2022-09-29 15:05:01 +0100

    translator miss error

--- server/mapper/translator/superscript_translator.py
+++ server/mapper/translator/superscript_translator.py
@@ -1,12 +1,13 @@
 def translate_superscript(string, lang):
-    if lang == 'fr':
-        if string.__contains__('re '):
-            return string.replace(
-                're ', '\N{MODIFIER LETTER SMALL R}\N{MODIFIER LETTER SMALL E}\N{NO-BREAK SPACE}')
-        if string.__contains__('er '):
-            return string.replace(
-                'er ', '\N{MODIFIER LETTER SMALL E}\N{MODIFIER LETTER SMALL R}\N{NO-BREAK SPACE}')
-        if string.__contains__('e '):
-            return string.replace(
-                'e ', '\N{MODIFIER LETTER SMALL E}\N{NO-BREAK SPACE}')
+    if string is not None:
+        if lang == 'fr':
+            if 're ' in string:
+                return string.replace(
+                    're ', '\N{MODIFIER LETTER SMALL R}\N{MODIFIER LETTER SMALL E}\N{NO-BREAK SPACE}')
+            if 'er ' in string:
+                return string.replace(
+                    'er ', '\N{MODIFIER LETTER SMALL E}\N{MODIFIER LETTER SMALL R}\N{NO-BREAK SPACE}')
+            if 'e ' in string:
+                return string.replace(
+                    'e ', '\N{MODIFIER LETTER SMALL E}\N{NO-BREAK SPACE}')
     return string
