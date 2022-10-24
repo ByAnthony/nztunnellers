423ba25511452725509cd1292c2ac1d091d6e641
ByAnthony <anthony.byledbal@icloud.com>
2022-10-24 20:21:51 +0100

    correct month name

--- server/models/helpers/date_helpers.py
+++ server/models/helpers/date_helpers.py
@@ -33,7 +33,7 @@ def find_day(date: str) -> str:
 
 
 def format_to_day_and_month(date: Optional[date], lang) -> Optional[str]:
-    months = {'1': {'en': 'January', 'fr': 'janvier'}, '2': {'en': 'Feburary', 'fr': 'février'}, '3': {'en': 'March', 'fr': 'mars'}, '4': {'en': 'April', 'fr': 'avril'}, '5': {'en': 'May', 'fr': 'mai'}, '6': {'en': 'June', 'fr': 'juin'},
+    months = {'1': {'en': 'January', 'fr': 'janvier'}, '2': {'en': 'February', 'fr': 'février'}, '3': {'en': 'March', 'fr': 'mars'}, '4': {'en': 'April', 'fr': 'avril'}, '5': {'en': 'May', 'fr': 'mai'}, '6': {'en': 'June', 'fr': 'juin'},
               '7': {'en': 'July', 'fr': 'juillet'}, '8': {'en': 'August', 'fr': 'août'}, '9': {'en': 'September', 'fr': 'septembre'}, '10': {'en': 'October', 'fr': 'octobre'}, '11': {'en': 'November', 'fr': 'novembre'}, '12': {'en': 'December', 'fr': 'décembre'}}
 
     if date is not None:
