ce5fbc445f40d9e8e0a9230ec1ff37362dba159f
ByAnthony <anthony.byledbal@icloud.com>
2022-11-04 17:24:25 +0000

    add more tests for source helpers

---
+++ server/test/models/formatter/test_month_year_converter.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# from ....models.helpers import month_year_converter
+
+
+# def test_if_month_is_none_and_lang_en_returns_none():
+#     assert month_year_converter.convert_month_year(None, "en") == None
+
+
+# def test_if_month_is_none_and_lang_fr_returns_none():
+#     assert month_year_converter.convert_month_year(None, "fr") == None
+
+
+# def test_if_month_is_equal_to_1_and_lang_en_returns_month_in_english():
+#     assert month_year_converter.convert_month_year("1", "en") == "1 month"
+
+
+# def test_if_month_is_equal_to_1_and_lang_fr_returns_months_in_french():
+#     assert month_year_converter.convert_month_year("1", "fr") == "1 mois"
+
+
+# def test_if_month_is_less_than_24_months_and_lang_en_returns_months_in_english():
+#     assert month_year_converter.convert_month_year("15", "en") == "15 months"
+
+
+# def test_if_month_is_less_than_24_months_and_lang_fr_returns_months_in_french():
+#     assert month_year_converter.convert_month_year("15", "fr") == "15 mois"
+
+
+# def test_if_month_is_more_than_24_months_and_lang_en_returns_years_in_english():
+#     assert month_year_converter.convert_month_year("24", "en") == "2 years"
+
+
+# def test_if_month_is_more_than_24_months_and_lang_fr_returns_years_in_french():
+#     assert month_year_converter.convert_month_year("250", "fr") == "20 ans"
