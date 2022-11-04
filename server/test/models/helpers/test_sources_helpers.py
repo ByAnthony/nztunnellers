7f086e0cb8571fa5397e21a2074e30139d198591
ByAnthony <anthony.byledbal@icloud.com>
2022-11-04 18:42:12 +0000

    add more tests for source helpers

--- server/test/models/helpers/test_sources_helpers.py
+++ server/test/models/helpers/test_sources_helpers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from server.models.sources import NewZealandArchives
-from server.models.helpers.sources_helpers import map_nz_archives, get_awmm
+from server.models.sources import NewZealandArchives, NominalRoll
+from server.models.helpers.sources_helpers import (
+    map_nz_archives,
+    get_awmm,
+    get_nominal_roll,
+)
 
 
 actual_nz_archives_1 = NewZealandArchives("AABK 18805 W5520 1/0006561", "22280535")
@@ -37,3 +41,67 @@ def test_get_awmm():
         get_awmm("fake_reference")
         == "https://www.aucklandmuseum.com/war-memorial/online-cenotaph/record/fake_reference"
     )
+
+
+volume = "III"
+roll = "75"
+page = "3"
+
+en = "en"
+fr = "fr"
+
+no_break_space = "\N{NO-BREAK SPACE}"
+roll_number_col = {"en": "Roll No.", "fr": "Liste n\N{DEGREE SIGN}"}
+
+title_1919 = {
+    "en": "Nominal Rolls of New Zealand Expeditionary Force",
+    "fr": "Liste nominative du corps expéditionnaire néo-zélandais",
+}
+title_1916 = {
+    "en": "Nominal Roll of New Zealand Expeditionary Force, 1915. New Zealand Engineers Tunnelling Company",
+    "fr": "Liste nominative du corps expéditionnaire néo-zélandais, 1915. Compagnie de tunneliers",
+}
+
+
+def test_if_volume_and_roll_are_not_none_and_lang_en_returns_nominal_roll():
+    assert get_nominal_roll(volume, roll, page, en) == NominalRoll(
+        title_1919[en],
+        "Wellington",
+        "Government Printer",
+        "1914-1919",
+        "p.{}{}".format(no_break_space, page),
+        "Volume{}{}".format(no_break_space, volume),
+        "{}{}".format(roll_number_col[en], roll),
+    )
+
+
+# def test_if_volume_and_roll_are_not_none_and_lang_fr_returns_nominal_roll():
+#     assert get_nominal_roll(volume, roll, page, fr) == {
+#         "title": title_1919[fr],
+#         "town": "Wellington",
+#         "publisher": "Government Printer",
+#         "date": "1914-1919",
+#         "volume": "Volume{}{}".format(no_break_space, volume),
+#         "roll": "{}{}{}".format(roll_number_col[fr], no_break_space, roll),
+#         "page": "p.{}{}".format(no_break_space, page),
+#     }
+
+
+# def test_if_volume_and_roll_are_none_and_lang_en_returns_nominal_roll():
+#     assert get_nominal_roll(None, None, page, en) == {
+#         "title": title_1916[en],
+#         "town": "Wellington",
+#         "publisher": "Government Printer",
+#         "date": "1916",
+#         "page": "p.{}{}".format(no_break_space, page),
+#     }
+
+
+# def test_if_volume_and_roll_are_none_and_lang_fr_returns_nominal_roll():
+#     assert get_nominal_roll(None, None, page, fr) == {
+#         "title": title_1916[fr],
+#         "town": "Wellington",
+#         "publisher": "Government Printer",
+#         "date": "1916",
+#         "page": "p.{}{}".format(no_break_space, page),
+#     }
