692d576820f7025770cfa5d1108404c6d1238f73
ByAnthony <anthony.byledbal@icloud.com>
2023-03-05 13:12:25 +0000

    add died to profile

--- client/src/components/DiaryBirthInfo/DiaryBirthInfo.tsx
+++ client/src/components/DiaryBirthInfo/DiaryBirthInfo.tsx
@@ -21,7 +21,7 @@ export function DiaryBirth({ birth }: Props) {
   );
 
   const displayBirthInfo = (birthCountry: string | null, birthDate: Date) => (
-    <div className={STYLES['fullwidth-secondary-card']}>
+    <div className={STYLES['fullwidth-main-card']}>
       { displayBirthCountry(birthCountry) }
       { displayBirthDate(birthDate) }
     </div>
--- client/src/components/DiaryDied/DiaryDied.tsx
+++ client/src/components/DiaryDied/DiaryDied.tsx
@@ -7,11 +7,13 @@ type Props = {
 }
 
 export function DiaryDied({ warDeath, afterWarDeath }: Props) {
+  const title = <p>Died</p>;
+
   const displayDeathInfo = (diedDuringWar: Death | null, diedAfterWar: PostWarDeath | null) => {
     if (diedDuringWar !== null && diedAfterWar === null) {
       return (
-        <div className={STYLES['fullwidth-secondary-card']}>
-          <p>Died</p>
+        <div className={STYLES['fullwidth-main-card']}>
+          { title }
           <span>
             { diedDuringWar.date.dayMonth !== null && diedDuringWar.date.year !== null
               ? `${diedDuringWar.date.dayMonth} ${diedDuringWar.date.year}`
@@ -22,8 +24,8 @@ export function DiaryDied({ warDeath, afterWarDeath }: Props) {
     }
     if (diedDuringWar === null && diedAfterWar !== null) {
       return (
-        <div className={STYLES['fullwidth-secondary-card']}>
-          <p>Died</p>
+        <div className={STYLES['fullwidth-main-card']}>
+          { title }
           <span>
             { diedAfterWar.date.dayMonth !== null && diedAfterWar.date.year !== null
               ? `${diedAfterWar.date.dayMonth} ${diedAfterWar.date.year}`
