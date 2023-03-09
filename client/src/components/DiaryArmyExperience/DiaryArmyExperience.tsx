7dcc4a8e652db14e99a697b4675679cb1448831e
ByAnthony <anthony.byledbal@icloud.com>
2023-03-09 09:36:27 +0000

    add button for wwi experience

--- client/src/components/DiaryArmyExperience/DiaryArmyExperience.tsx
+++ client/src/components/DiaryArmyExperience/DiaryArmyExperience.tsx
@@ -1,5 +1,6 @@
 import { ArmyExperience } from '../../types/tunneller';
 import STYLES from '../ProfileDiary/ProfileDiary.module.scss';
+import STYLES_WWI from './DiaryArmyExperience.module.scss';
 
 type Props = {
     armyExperience: ArmyExperience[],
@@ -11,6 +12,7 @@ export function DiaryArmyExperience({ armyExperience }: Props) {
       const displayConflict = experience.conflict !== null ? experience.conflict : null;
       const displayDuration = experience.duration !== null ? experience.duration : null;
       const displayCountry = experience.country !== null ? experience.country : null;
+
       const displayDurationAndCountry = () => {
         if (experience.duration !== null && experience.country !== null) {
           return `${displayDuration} in ${displayCountry}`;
@@ -58,9 +60,6 @@ export function DiaryArmyExperience({ armyExperience }: Props) {
     if (armyExperience.length > 0) {
       return (
         <>
-          <div className={STYLES['fullwidth-main-card']}>
-            <span>Army Experience</span>
-          </div>
           { displayExperience() }
         </>
       );
@@ -70,7 +69,17 @@ export function DiaryArmyExperience({ armyExperience }: Props) {
 
   return (
     <>
+      <div className={STYLES['fullwidth-main-card']}>
+        <span>Army Experience</span>
+      </div>
       { displayArmyExperience(armyExperience) }
+      <div className={STYLES_WWI['war-service']}>
+        <div>
+          <p>World War I (1914-1918)</p>
+          <span>New Zealand Engineers Tunnelling Company</span>
+        </div>
+        <div className={STYLES.arrow}>&rarr;</div>
+      </div>
     </>
   );
 }
