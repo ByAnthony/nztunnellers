1c3403750a0626476ecfd9781decec5bac538f6e
ByAnthony <anthony.byledbal@icloud.com>
2023-04-01 08:02:58 +0100

    add links to footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -8,46 +8,27 @@ export function Footer() {
       </div>
       <div>
         <div className={STYLES['footer-links-container']}>
-          <div>
-            <p>The Company History</p>
+          <div className={STYLES['footer-links']}>
             <ul>
               <li>
-                <a href="/history/call-to-pick-and-shovel/" aria-label="The history of the Company, Chapter 1: Call To Pick & Shovel.">Call To Pick & Shovel</a>
+                <a href="/history/" aria-label="History">History</a>
               </li>
               <li>
-                <a href="/history/the-way-to-war/" aria-label="The history of the Company, Chapter 2: The Way To War.">The Way To War</a>
-              </li>
-              <li>
-                <a href="/history/beneath-artois-fields/" aria-label="The history of the Company, Chapter 3: Beneath Artois Fields.">Beneath Artois Fields</a>
-              </li>
-              <li>
-                <a href="/history/tunnelling-under-arras/" aria-label="The history of the Company, Chapter 4: Tunnelling Under Arras.">Tunnelling Under Arras</a>
-              </li>
-              <li>
-                <a href="/history/always-digging/" aria-label="The history of the Company, Chapter 5: Always Digging.">Always Digging</a>
-              </li>
-              <li>
-                <a href="/history/bridging-in-the-end/" aria-label="The history of the Company, Chapter 6: Bridging In The End.">Bridging In The End</a>
-              </li>
-              <li>
-                <a href="/history/after-the-armistice/" aria-label="The history of the Company, Chapter 7: After The Armistice.">After The Armistice</a>
+                <a href="/roll/" aria-label="Company Roll">Company Roll</a>
               </li>
             </ul>
+
           </div>
-          <div>
-            <p>Tunellers Stories</p>
-            <ul>
-              <li>
-                <a href="/roll/" aria-label="The Company Roll: a list of the men who enlisted in the Company.">Company Roll</a>
-              </li>
-            </ul>
-            <p>Website</p>
+          <div className={STYLES['footer-links']}>
             <ul>
               <li>
                 <a href="/about-us/" aria-label="About Us">About Us</a>
               </li>
               <li>
                 <a href="/contact-us/" aria-label="Contact Us">Contact Us</a>
+              </li>
+              <li>
+                <a href="/help-us/" aria-label="Help Us">Help Us</a>
               </li>
             </ul>
           </div>
