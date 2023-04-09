06c652ebf984baa503be96f17f2c6619de805f17
ByAnthony <anthony.byledbal@icloud.com>
2023-04-09 10:58:46 +0100

    update footer with cards

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -8,13 +8,23 @@ export function Footer() {
   return (
     <div className={STYLES.footer}>
       <div className={STYLES.support}>
-        <p>Support</p>
-        <div className={STYLES['support-logos']}>
-          <a href="https://www.univ-artois.fr/artois-university" aria-label="Go to The Artois University website."><img src="../images/support/logo-univ-artois-blanc_0.png" alt="" /></a>
-          <a href="https://www.irsem.fr/en/" aria-label="Go to The Institute for Strategic Research website."><img src="../images/support/irsem-white.png" alt="" /></a>
+        <div className={STYLES['fullwidth-main-card']}>
+          <p>New Zealand Tunnellers Website</p>
+          <span>Supported by</span>
+        </div>
+        <div className={STYLES['halfwidth-cards-container']}>
+          <div className={STYLES['halfwidth-secondary-card']}>
+            <a href="https://www.univ-artois.fr/artois-university" aria-label="Go to The Artois University website."><img src="../images/support/logo-univ-artois-blanc_0.png" alt="" /></a>
+          </div>
+          <div className={STYLES['halfwidth-secondary-card']}>
+            <a href="https://www.irsem.fr/en/" aria-label="Go to The Institute for Strategic Research website."><img src="../images/support/irsem-white.png" alt="" /></a>
+          </div>
         </div>
       </div>
       <div className={STYLES.links}>
+        <div className={STYLES['fullwidth-main-card']}>
+          <p>Contact Us</p>
+        </div>
         <button type="button" className={STYLES['scroll-top']} onClick={handleClick} aria-label="Go back to the top of the page.">&uarr;</button>
       </div>
     </div>
