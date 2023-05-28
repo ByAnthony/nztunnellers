d819d51ba8d2d6bb653a95fa254a48148d751c75
Anthony Byledbal <anthony.byledbal@icloud.com>
2023-05-28 17:49:47 +0100

    add mockPreWarYears

--- client/src/utils/mocks/mockPreWarYears.ts
+++ client/src/utils/mocks/mockPreWarYears.ts
@@ -1,1 +1,28 @@
-export {};
+import { ArmyExperience, Employment, PreWayYears } from '../../types/tunneller';
+
+export const mockArmyExperience = (overrides: Partial<ArmyExperience>
+    | undefined = undefined): ArmyExperience => ({
+  unit: 'NZ Infantry',
+  country: 'New Zealand',
+  conflict: null,
+  duration: null,
+  ...overrides,
+});
+
+export const mockEmployment = (overrides: Partial<Employment>
+    | undefined = undefined): Employment => ({
+  occupation: 'Goldminer',
+  employer: 'Goldmining Company',
+  ...overrides,
+});
+
+export const mockPreWarYears = (overrides: Partial<PreWayYears>
+    | undefined = undefined): PreWayYears => ({
+  armyExperience: [mockArmyExperience()],
+  employment: mockEmployment(),
+  residence: null,
+  maritalStatus: null,
+  wife: null,
+  religion: null,
+  ...overrides,
+});
