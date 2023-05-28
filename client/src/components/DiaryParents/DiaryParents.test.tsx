1481f9da995021f310bdba84e8a84bc81c4c9ba7
Anthony Byledbal <anthony.byledbal@icloud.com>
2023-05-28 17:34:27 +0100

    add tests for parents

---
+++ client/src/components/DiaryParents/DiaryParents.test.tsx
@@ -0,0 +1,62 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { DiaryParents } from './DiaryParents';
+import { mockParents } from '../../utils/mocks/mockOrigins';
+
+const component = (
+  <DiaryParents parents={mockParents()} />
+);
+
+test('renders the component correctly', () => {
+  const { asFragment } = render(component);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('renders parents when known', () => {
+  render(component);
+
+  expect(screen.getByText('Parents')).toBeInTheDocument();
+  expect(screen.getByText('Mother')).toBeInTheDocument();
+  expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+  expect(screen.getByText('Father')).toBeInTheDocument();
+  expect(screen.getByText('John Doe')).toBeInTheDocument();
+});
+
+test('renders only mother when father unknown', () => {
+  const componentWithMotherOnly = (<DiaryParents parents={mockParents({ father: null })} />);
+
+  render(componentWithMotherOnly);
+
+  expect(screen.queryByText('Parents')).not.toBeInTheDocument();
+  expect(screen.getByText('Parent')).toBeInTheDocument();
+  expect(screen.getByText('Mother')).toBeInTheDocument();
+  expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+  expect(screen.queryByText('Father')).not.toBeInTheDocument();
+  expect(screen.queryByText('John Doe')).not.toBeInTheDocument();
+});
+
+test('renders only father when father unknown', () => {
+  const componentWithMotherOnly = (<DiaryParents parents={mockParents({ mother: null })} />);
+
+  render(componentWithMotherOnly);
+
+  expect(screen.queryByText('Parents')).not.toBeInTheDocument();
+  expect(screen.getByText('Parent')).toBeInTheDocument();
+  expect(screen.queryByText('Mother')).not.toBeInTheDocument();
+  expect(screen.queryByText('Jane Doe')).not.toBeInTheDocument();
+  expect(screen.getByText('Father')).toBeInTheDocument();
+  expect(screen.getByText('John Doe')).toBeInTheDocument();
+});
+
+test('does not render the component when parents are unknown', () => {
+  const { container } = render(
+    <DiaryParents parents={mockParents({
+      mother: null,
+      father: null,
+    })}
+    />,
+  );
+
+  expect(container).toBeEmptyDOMElement();
+});
