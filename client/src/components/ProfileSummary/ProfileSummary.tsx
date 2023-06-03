9e2025f9d509260f4c618c822d1ed98c6815069a
Anthony Byledbal <anthony.byledbal@icloud.com>
2023-06-03 17:04:57 +0100

    refactoring jsx

--- client/src/components/ProfileSummary/ProfileSummary.tsx
+++ client/src/components/ProfileSummary/ProfileSummary.tsx
@@ -7,44 +7,44 @@ type props = {
     image: Image | null,
 }
 
-export function ProfileSummary({ embarkationUnit, enlistment, image }: props) {
-  const displayImage = (imageUrl: string | undefined) => (
-    imageUrl ? (
-      <div className={STYLES['image-card']}>
-        <img src={`/images/roll/tunnellers/${image?.url}`} alt="" />
-      </div>
-    ) : null);
+function RenderImage({ imageUrl }: {imageUrl: string | undefined}) {
+  return imageUrl ? (
+    <div className={STYLES['image-card']}>
+      <img src={`/images/roll/tunnellers/${imageUrl}`} alt="" />
+    </div>
+  ) : null;
+}
 
-  const displayUnit = (
-    unit: string,
-    section: string | null,
-  ) => {
-    if (unit === 'Main Body' && section) {
-      return (
-        <>
-          <div className={STYLES['fullwidth-main-card']}>
-            <p>Unit</p>
-            <span>{ unit }</span>
-          </div>
-          <div className={STYLES['fullwidth-main-card']}>
-            <p>Section</p>
-            <span>{ section }</span>
-          </div>
-        </>
-      );
-    }
+function RenderUnit({ unit, section }:
+  {unit: string,
+  section: string | null,}) {
+  if (unit === 'Main Body' && section) {
     return (
-      <div className={STYLES['fullwidth-main-card']}>
-        <p>Unit</p>
-        <span>{ unit }</span>
-      </div>
+      <>
+        <div className={STYLES['fullwidth-main-card']}>
+          <p>Unit</p>
+          <span>{ unit }</span>
+        </div>
+        <div className={STYLES['fullwidth-main-card']}>
+          <p>Section</p>
+          <span>{ section }</span>
+        </div>
+      </>
     );
-  };
+  }
+  return (
+    <div className={STYLES['fullwidth-main-card']}>
+      <p>Unit</p>
+      <span>{ unit }</span>
+    </div>
+  );
+}
 
+export function ProfileSummary({ embarkationUnit, enlistment, image }: props) {
   return (
     <div className={STYLES.overview}>
-      { displayImage(image?.url) }
-      { displayUnit(embarkationUnit.detachment, embarkationUnit.section) }
+      <RenderImage imageUrl={image?.url} />
+      <RenderUnit unit={embarkationUnit.detachment} section={embarkationUnit.section} />
       <div className={STYLES['halfwidth-cards-container']}>
         <div className={STYLES['halfwidth-secondary-card']}>
           <div className={STYLES['halfwidth-secondary-card-title']}><p>Rank</p></div>
