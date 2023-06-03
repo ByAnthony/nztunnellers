885105d9c3f591e6cc571fb70f06c4d96df78889
Anthony Byledbal <anthony.byledbal@icloud.com>
2023-06-03 17:13:26 +0100

    refactoring jsx

--- client/src/components/DiaryMedal/DiaryMedal.tsx
+++ client/src/components/DiaryMedal/DiaryMedal.tsx
@@ -6,38 +6,45 @@ type props = {
     medals: Medal[] | [],
 }
 
-export function DiaryMedal({ medals }: props) {
-  const displayBritishWarAndVictoryMedals = (medalsList: Medal[] | []) => {
-    const filteredMedals = medalsList.filter((medal) => medal.name === 'British War Medal' || medal.name === 'Victory Medal');
-    return filteredMedals.map((medal) => (
-      <div key={filteredMedals.indexOf(medal)} className={STYLES['halfwidth-secondary-card']}>
-        <div className={STYLES['halfwidth-secondary-card-title']}><p><img src={`/images/roll/medals/${medal.image}`} alt="" width={40} /></p></div>
-        <div><span>{ medal.name }</span></div>
-      </div>
-    ));
-  };
-
-  const displayOtherMedals = (medalsList: Medal[] | []) => {
-    const filteredMedals = medalsList.filter((medal) => medal.name !== 'British War Medal' && medal.name !== 'Victory Medal');
-    const otherMedals = filteredMedals.map((medal) => {
-      const displayCountry = (medal.country !== 'United Kingdom') ? `(${medal.country})` : '';
-      return (
-        <div key={filteredMedals.indexOf(medal)} className={OTHER_MEDALS_STYLES['other-medal']}>
-          <div><p><img src={`/images/roll/medals/${medal.image}`} alt="" width={40} /></p></div>
-          <div><span>{`${medal.name} ${displayCountry}`}</span></div>
-          <div className={OTHER_MEDALS_STYLES.citation}><span>{`${medal.citation}.`}</span></div>
+function BritishWarAndVictoryMedals({ medalsList }: {medalsList: Medal[] | []}) {
+  const filteredMedals = medalsList.filter((medal) => medal.name === 'British War Medal' || medal.name === 'Victory Medal');
+  return (
+    <>
+      {filteredMedals.map((medal) => (
+        <div key={filteredMedals.indexOf(medal)} className={STYLES['halfwidth-secondary-card']}>
+          <div className={STYLES['halfwidth-secondary-card-title']}><p><img src={`/images/roll/medals/${medal.image}`} alt="" width={40} /></p></div>
+          <div><span>{ medal.name }</span></div>
         </div>
-      );
-    });
-    return otherMedals;
-  };
+      ))}
+    </>
+  );
+}
 
+function OtherMedals({ medalsList }: {medalsList: Medal[] | []}) {
+  const filteredMedals = medalsList.filter((medal) => medal.name !== 'British War Medal' && medal.name !== 'Victory Medal');
+  return (
+    <>
+      {filteredMedals.map((medal) => {
+        const displayCountry = (medal.country !== 'United Kingdom') ? `(${medal.country})` : '';
+        return (
+          <div key={filteredMedals.indexOf(medal)} className={OTHER_MEDALS_STYLES['other-medal']}>
+            <div><p><img src={`/images/roll/medals/${medal.image}`} alt="" width={40} /></p></div>
+            <div><span>{`${medal.name} ${displayCountry}`}</span></div>
+            <div className={OTHER_MEDALS_STYLES.citation}><span>{`${medal.citation}.`}</span></div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export function DiaryMedal({ medals }: props) {
   return (
     <>
       <div className={STYLES['halfwidth-cards-container']}>
-        { displayBritishWarAndVictoryMedals(medals) }
+        <BritishWarAndVictoryMedals medalsList={medals} />
       </div>
-      { displayOtherMedals(medals) }
+      <OtherMedals medalsList={medals} />
     </>
   );
 }
