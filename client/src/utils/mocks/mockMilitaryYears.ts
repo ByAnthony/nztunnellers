6a58c770c6902ebca70011885f755a21c89a8435
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2023-06-04 21:17:28 +0100

    add tests for death date

--- client/src/utils/mocks/mockMilitaryYears.ts
+++ client/src/utils/mocks/mockMilitaryYears.ts
@@ -1,1 +1,34 @@
-export {};
+import {
+  Date, DeathPlace, Death, DeathCause, Cemetery,
+} from '../../types/tunneller';
+
+const mockDate: Date = {
+  year: '1926',
+  dayMonth: '13 October',
+};
+
+const mockPlace: DeathPlace = {
+  location: 'Telegraph Hill',
+  town: 'Arras',
+  country: 'France',
+};
+
+const mockCause: DeathCause = {
+  type: 'Killed in action',
+  circumstances: 'Dead wounded by shell fire',
+};
+
+const mockCemetery: Cemetery = {
+  name: 'Faubourg d\'Amiens',
+  location: 'Arras',
+  country: 'France',
+  graveReference: 'IE 18',
+};
+
+export const mockDeath: Death = {
+  date: mockDate,
+  place: mockPlace,
+  cause: mockCause,
+  cemetery: mockCemetery,
+  ageAtDeath: 89,
+};
--- client/src/utils/mocks/mockPostServiceYears.ts
+++ client/src/utils/mocks/mockPostServiceYears.ts
@@ -1,1 +1,11 @@
-export {};
+import { PostServiceYears, PostWarDeath } from '../../types/tunneller';
+import { mockDeath } from './mockMilitaryYears';
+
+export const mockAfterWarDeath: PostWarDeath = {
+  deathWarInjury: false,
+  ...mockDeath,
+};
+
+export const mockPostServiceYears: PostServiceYears = {
+  death: mockAfterWarDeath,
+};
