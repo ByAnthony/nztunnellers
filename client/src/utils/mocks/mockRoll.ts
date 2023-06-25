ccce63f533fe7d5c5b24944a1ab9e054ab7d41ce
ByAnthony <anthony.byledbal@icloud.com>
2023-06-25 12:33:06 +0100

    add mockRoll

---
+++ client/src/utils/mocks/mockRoll.ts
@@ -0,0 +1,13 @@
+import { mockId } from './mockProfile';
+
+export const mockName = {
+  forename: 'John',
+  surname: 'Doe',
+};
+
+export const mockDetails = {
+  id: mockId,
+  name: mockName,
+  birth: '1886',
+  death: '1952',
+};
