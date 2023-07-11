42ee7b649791ea3d70a7ad58034ac987f139f0fc
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2023-07-11 21:44:41 +0100

    next button

--- client/src/components/History/Article/NextChapterButton/NextChapterButton.tsx
+++ client/src/components/History/Article/NextChapterButton/NextChapterButton.tsx
@@ -12,11 +12,11 @@ export function NextChapterButton({ next }: Props) {
         <a
           href={`/history/${next.url}`}
           className={STYLES['button-chapter']}
-          aria-label={`Go to Chapter ${next.chapter}: ${next.title}`}
+          aria-label={`Go to Chapter ${next.chapter}: ${next.title.replace('\\', ' ')}`}
         >
           <div>
             <p>{`Chapter ${next.chapter}`}</p>
-            <span>{next.title}</span>
+            <span>{next.title.replace('\\', ' ')}</span>
           </div>
           <div className={STYLES.arrow}>&rarr;</div>
         </a>
