056a6bbd4176b69edfd7659cc73d94604fa081ae
ByAnthony <anthony.byledbal@icloud.com>
2023-08-06 15:38:58 +0100

    add test for next chapter button

---
+++ client/src/components/History/Article/NextChapterButton/NextChapterButton.test.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { mockNextButton } from '../../../../utils/mocks/mockArticle';
+import { NextChapterButton } from './NextChapterButton';
+
+const component = (
+  <NextChapterButton chapter={mockNextButton} />
+);
+
+test('renders the component correctly', () => {
+  const { asFragment } = render(component);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('renders next chapter button correctly', () => {
+  render(component);
+
+  expect(screen.getByRole('link')).toHaveAttribute('href', '/history/my-path-to-next-chapter/');
+  expect(screen.getByRole('link')).toHaveTextContent(/Next Chapter/);
+  expect(screen.getByRole('link')).toHaveTextContent(/Chapter 3/);
+});
+
+test('do not render next chapter button if null', () => {
+  const { container } = render(
+    <NextChapterButton chapter={undefined} />,
+  );
+
+  expect(container).toBeEmptyDOMElement();
+});
--- client/src/utils/mocks/mockArticle.ts
+++ client/src/utils/mocks/mockArticle.ts
@@ -1,4 +1,4 @@
-import { Image, Section } from '../../types/article';
+import { Image, Next, Section } from '../../types/article';
 
 export const mockTitle: string = 'My Awesome\\Article Title';
 
@@ -12,6 +12,12 @@ export const mockImage: Image = {
 export const mockSection: Section = {
   title: 'Section Title',
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea *commodo consequat*. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\\n\\nLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat[1.](#footnote_1). Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\\n\\n(6--July 2023)',
+};
+
+export const mockNextButton: Next = {
+  url: 'my-path-to-next-chapter/',
+  title: 'Next\\Chapter',
+  chapter: 3,
 };
 
 export const mockNotes: string = '[1.](#reference_1) Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\\n\\n[2.](#reference_2) Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
