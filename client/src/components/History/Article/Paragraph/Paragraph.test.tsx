e6da062c43374054973f1f4d1a42733e629742ab
ByAnthony <anthony.byledbal@icloud.com>
2023-08-06 15:31:08 +0100

    add test for notes

--- client/src/components/History/Article/Paragraph/Paragraph.test.tsx
+++ client/src/components/History/Article/Paragraph/Paragraph.test.tsx
@@ -17,8 +17,6 @@ test('renders title correctly', () => {
   render(component);
 
   expect(screen.getByText(/Section Title/)).toBeInTheDocument();
-  expect(screen.getAllByRole('link')[0]).toHaveAttribute('href', '#footnote_1');
-  expect(screen.getAllByRole('link')[0]).toHaveAttribute('id', 'reference_1');
-  expect(screen.getAllByRole('link')[1]).toHaveAttribute('href', '#reference_1');
-  expect(screen.getAllByRole('link')[1]).toHaveAttribute('id', 'footnote_1');
+  expect(screen.getByRole('link')).toHaveAttribute('href', '#footnote_1');
+  expect(screen.getByRole('link')).toHaveAttribute('id', 'reference_1');
 });
