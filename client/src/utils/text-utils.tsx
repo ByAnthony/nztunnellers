08ad05ed0e6c5e14d795efe7e1bba612c74fbe00
ByAnthony <anthony.byledbal@icloud.com>
2023-08-07 13:04:31 +0100

    fix mockParagraph

--- client/src/utils/text-utils.tsx
+++ client/src/utils/text-utils.tsx
@@ -14,21 +14,22 @@ export const formatText = (text: string) => {
       if (segment && segment.startsWith('[') && segment.endsWith(')')) {
         const linkText = segment.slice(1, -1);
         const [label, url] = linkText.split('](');
-        const number = label.slice(0, -1);
         if (url.includes('footnote')) {
           return (
-            <a key={segmentKey} href={url} id={`reference_${number}`}>
+            <a key={segmentKey} href={url} id={`reference_${label}`}>
               [
               {label}
               ]
             </a>
           );
         }
-        return (
-          <a key={segmentKey} href={url} id={`footnote_${number}`}>
-            {label}
-          </a>
-        );
+        if (url.includes('reference')) {
+          return (
+            <a key={segmentKey} href={url} id={`footnote_${label.slice(0, -1)}`}>
+              {label}
+            </a>
+          );
+        }
       }
       return <span key={segmentKey}>{segment}</span>;
     });
