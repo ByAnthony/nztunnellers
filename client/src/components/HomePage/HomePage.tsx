980d2613e72782460560789f8d8a2cec70d219f7
ByAnthony <anthony.byledbal@icloud.com>
2024-02-11 08:33:43 +0000

    add wrapper to chapter cards

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -22,33 +22,34 @@ export function HomePage() {
         { isSuccess && (
           <div className={STYLES['homepage-container']}>
             <h2 id="history">History of the Company</h2>
-            <div className={STYLES['chapter-cards']}>
-              {data.map((article) => {
-                const divStyle = {
-                  backgroundImage: `url(../images/history/${article.image})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center center',
-                };
-
-                return (
-                  <a
-                    href={`/history/${article.url}`}
-                    className={STYLES['link-button']}
-                    aria-label={`Go to Chapter ${article.chapter}: ${article.title.replace('\\', ' ')}`}
-                  >
-                    <div className={STYLES['chapter-card']} key={data.indexOf(article)} style={divStyle}>
-                      <div className={STYLES['chapter-card-dimmer']}>
-                        <div className={STYLES['chapter-card-content']}>
-                          <div>
-                            <p>{article.title.replace('\\', ' ')}</p>
-                            <span>{`Chapter ${article.chapter}`}</span>
+            <div className={STYLES['chapter-cards-wrapper']}>
+              <div className={STYLES['chapter-cards']}>
+                {data.map((article) => {
+                  const divStyle = {
+                    backgroundImage: `url(../images/history/${article.image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                  };
+                  return (
+                    <a
+                      href={`/history/${article.url}`}
+                      className={STYLES['link-button']}
+                      aria-label={`Go to Chapter ${article.chapter}: ${article.title.replace('\\', ' ')}`}
+                    >
+                      <div className={STYLES['chapter-card']} key={data.indexOf(article)} style={divStyle}>
+                        <div className={STYLES['chapter-card-dimmer']}>
+                          <div className={STYLES['chapter-card-content']}>
+                            <div>
+                              <p>{article.title.replace('\\', ' ')}</p>
+                              <span>{`Chapter ${article.chapter}`}</span>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </a>
-                );
-              })}
+                    </a>
+                  );
+                })}
+              </div>
             </div>
           </div>
         )}
