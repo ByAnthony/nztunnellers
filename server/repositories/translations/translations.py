b08e5d04554d961ea0ff47efef1c7d23fd1a5f51
ByAnthony <anthony.byledbal@icloud.com>
2024-04-02 22:06:43 +0100

    remove init files

---
+++ server/repositories/translations/translations.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+attached_corps_col = {"en": "attached_corps.corps_en", "fr": "attached_corps.corps_fr"}
+
+
+birth_country_col = {"en": "birth_country.country_en", "fr": "birth_country.country_fr"}
+
+
+conflict_col = {"en": "conflict_name_en", "fr": "conflict_name_fr"}
+
+
+country_col = {"en": "country.country_en", "fr": "country.country_fr"}
+
+
+embarkation_unit_col = {
+    "en": "embarkation_unit.embarkation_unit_en",
+    "fr": "embarkation_unit.embarkation_unit_fr",
+}
+
+
+father_origin_col = {"en": "father_origin.country_en", "fr": "father_origin.country_fr"}
+
+
+marital_status_col = {
+    "en": "marital_status.marital_status_en",
+    "fr": "marital_status.marital_status_fr",
+}
+
+
+medal_citation_col = {
+    "en": "medal_citation.medal_citation_en",
+    "fr": "medal_citation.medal_citation_fr",
+}
+
+
+medal_name_col = {"en": "medal.medal_name_en", "fr": "medal.medal_name_fr"}
+
+
+mother_origin_col = {"en": "mother_origin.country_en", "fr": "mother_origin.country_fr"}
+
+
+occupation_col = {"en": "occupation.occupation_en", "fr": "occupation.occupation_fr"}
+
+
+posted_from_corps_col = {
+    "en": "posted_from_corps.corps_en",
+    "fr": "posted_from_corps.corps_fr",
+}
+
+
+rank_col = {"en": "rank_en", "fr": "rank_fr"}
+
+
+religion_col = {"en": "religion.religion_en", "fr": "religion.religion_fr"}
+
+
+section_col = {"en": "section.section_en", "fr": "section.section_fr"}
+
+
+training_location_type_col = {
+    "en": "training_location_type.training_location_type_en",
+    "fr": "training_location_type.training_location_type_fr",
+}
+
+
+transferred_to_col = {
+    "en": "transferred_to.transferred_to_en",
+    "fr": "transferred_to.transferred_to_fr",
+}
+
+death_location_col = {
+    "en": "death_location.death_location_en",
+    "fr": "death_location.death_location_fr",
+}
+
+death_country_col = {
+    "en": "death_country.country_en",
+    "fr": "death_country.country_fr",
+}
+
+death_cause_col = {
+    "en": "death_cause.death_cause_en",
+    "fr": "death_cause.death_cause_fr",
+}
+
+cemetery_col = {
+    "en": "cemetery.cemetery_name_en",
+    "fr": "cemetery.cemetery_name_fr",
+}
+
+cemetery_country_col = {
+    "en": "cemetery_country.country_en",
+    "fr": "cemetery_country.country_fr",
+}
+
+death_circumstances_col = {
+    "en": "death_circumstances.death_circumstances_en",
+    "fr": "death_circumstances.death_circumstances_fr",
+}
