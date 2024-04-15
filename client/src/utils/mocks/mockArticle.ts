a1e739169b5c71b2d4306528f96eb582affceca0
ByAnthony <anthony.byledbal@icloud.com>
2024-04-15 18:35:56 +0100

    add about-us test

--- client/src/utils/mocks/mockArticle.ts
+++ client/src/utils/mocks/mockArticle.ts
@@ -29,6 +29,11 @@ export const mockSectionTwo: Section = {
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea *commodo consequat*. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\\n\\nLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat[1](#footnote_1). Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\\n\\n(6--July 2023)',
 };
 
+export const mockSectionThree: Section = {
+  title: 'Section Title',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor [incididunt](http://www.loremipsum.com) ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea *commodo consequat*. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\\n\\nLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat[1](#footnote_1). Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\\n\\n(6--July 2023)',
+};
+
 export const mockNextButton: Next = {
   url: 'my-path-to-next-chapter/',
   title: 'Next\\Chapter',
@@ -50,6 +55,6 @@ export const mockArticle: Article = {
 export const mockAboutUs: AboutUs = {
   id: 'my-awesome-article',
   title: mockTitle,
-  section: [mockSectionOne, mockSectionTwo],
+  section: [mockSectionOne, mockSectionThree],
   image: [mockTopImage],
 };
