89e59123c172ae1226bf763f82d3ed959c8bd72a
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2024-05-17 15:32:57 +0100

    chapters scroll

--- client/src/components/HomePage/HistoryChapters/HistoryChapter.tsx
+++ client/src/components/HomePage/HistoryChapters/HistoryChapter.tsx
@@ -12,14 +12,14 @@ export function HistoryChapters({ articles }: Props) {
 
   const scrollLeft = () => {
     if (containerRef.current) {
-      const scrollAmount = containerRef.current.clientWidth / 3;
+      const scrollAmount = 301;
       containerRef.current.scrollLeft -= scrollAmount;
     }
   };
 
   const scrollRight = () => {
     if (containerRef.current) {
-      const scrollAmount = containerRef.current.clientWidth / 3;
+      const scrollAmount = 301;
       containerRef.current.scrollLeft += scrollAmount;
     }
   };
